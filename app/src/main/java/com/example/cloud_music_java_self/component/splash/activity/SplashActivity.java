fed7d8f5f21811c0f10b3ff9d685a147b6fc5a91
koen <2382047225@qq.com>
2024-12-24 16:07:35 +0800

    上一个修改没push全，😅

---
+++ app/src/main/java/com/example/cloud_music_java_self/component/splash/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.cloud_music_java_self.component.splash.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cloud_music_java_self.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
